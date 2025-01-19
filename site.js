5513263c6b6fe56f4afd838f2f034f8e9d2a9067
James-Lee Spaulding <300090312@fvtc.edu>
2025-01-19 16:22:17 -0600

    done with exercises

---
+++ site.js
@@ -0,0 +1,9 @@
+//For Exercises 1&2
+const mainContentSection = document.querySelector('h2')
+mainContentSection.style.color = 'red'
+
+
+//const forkLink = document.querySelector('footer div')
+const forkLink = document.querySelector('#fork')
+
+forkLink.style.textDecoration = 'underline'
